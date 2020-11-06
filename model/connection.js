5c619d93e5cadbf7bc8429778122833a19ae5cac
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2020-11-06 14:42:42 +0100

    creating files

--- model/connection.js
+++ model/connection.js
@@ -1,9 +1,12 @@
 const sequelize = require('./config')
 
+//import your models here
+
 const connect =  async () => {
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
+    // sync your models here
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
